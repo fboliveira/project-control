bfe9861c33f55f4744cdb44eaef8ceb3b02ffd5b
Fernando B Oliveira <fboliveira25@gmail.com>
2026-08-28 14:24:40 -0300

    feat: server parameters

--- python-fastapi/src/app/core/config.py
+++ python-fastapi/src/app/core/config.py
@@ -3,6 +3,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Config(BaseSettings):
     APP_NAME: str
     DATABASE_URL: str
+    SERVER_HOST: str
+    SERVER_PORT: int
 
     # Tell Pydantic to read from a .env file
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
