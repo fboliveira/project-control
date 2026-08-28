cd55ef6734852448ac9e784a43996347ab1c3d47
Fernando B Oliveira <fboliveira25@gmail.com>
2026-08-28 14:24:29 -0300

    feat: run with uvicorn

--- python-fastapi/src/main.py
+++ python-fastapi/src/main.py
@@ -1,3 +1,5 @@
+import uvicorn
+
 from fastapi import FastAPI
 from app.project import routes
 
@@ -7,4 +9,7 @@ config = Config()
 
 app = FastAPI(title=config.APP_NAME)
 app.include_router(routes.router)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host=config.SERVER_HOST, port=config.SERVER_PORT, reload=True)
 
