f9ca001e104932a847d6412e38328c2e8a76595c
Fernando B Oliveira <fboliveira25@gmail.com>
2026-09-08 16:19:17 -0300

    refactor: replace from sqlalchemy to sqlmodel

--- python-fastapi/src/app/core/database.py
+++ python-fastapi/src/app/core/database.py
@@ -1,17 +1,20 @@
 from collections.abc import Generator
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import DeclarativeBase, Session, sessionmaker
+from sqlmodel import Session, SQLModel, create_engine
 
 from config import Config
 
 config = Config()
 
-class Base(DeclarativeBase):
+class Base(SQLModel, table=True):
     pass
 
 engine = create_engine(config.DATABASE_URL, pool_pre_ping=True)
-SessionLocal = sessionmaker(bind=engine, autoflush=False, expire_on_commit=False)
+
+# Create the Tables
+SQLModel.metadata.create_all(engine)
+
+SessionLocal = Session(bind=engine, autoflush=False, expire_on_commit=False)
 
 def get_connection() -> Generator[Session, None, None]:
     with SessionLocal() as session:
