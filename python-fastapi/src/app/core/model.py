9fc99f3683b02ab6c5ba7a06be74ec16aa19400c
Fernando B Oliveira <fboliveira25@gmail.com>
2026-09-08 16:45:37 -0300

    fix: id Int | None
    The value of id will be None until we save it in the database, and then it will finally have a value.

--- python-fastapi/src/app/core/model.py
+++ python-fastapi/src/app/core/model.py
@@ -4,6 +4,7 @@ import datetime
 from sqlmodel import Field, SQLModel
 
 class ModelBase(SQLModel, table=True):
-    id: int = Field(default=None, primary_key=True)
+    # The value of id will be None until we save it in the database, and then it will finally have a value.
+    id: int | None = Field(default=None, primary_key=True)
     created_at: datetime = Field(default_factory=datetime.now)
     updated_at: datetime = Field(default_factory=datetime.now)
