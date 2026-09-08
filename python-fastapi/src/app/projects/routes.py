f6aa167f24337b1dafe8de86bb14a23111187fc5
Fernando B Oliveira <fboliveira25@gmail.com>
2026-09-08 18:05:24 -0300

    feat: get all service

--- python-fastapi/src/app/projects/routes.py
+++ python-fastapi/src/app/projects/routes.py
@@ -1,7 +1,10 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
+from app.projects.services import ProjectService
 
 router = APIRouter()
 
 @router.get('/projects')
-def get_all_projects():
-    return {"projects" : []}
+def get_all_projects(project_service: Annotated[ProjectService, Depends()]):
+    project_service.get_projects()
