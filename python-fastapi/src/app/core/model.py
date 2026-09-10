ed72784c139191c9c4e8c7eb48c5d51f1c0d5cd8
Fernando B Oliveira <fboliveira25@gmail.com>
2026-09-10 18:36:23 -0300

    fix: remove table=true from model base

--- python-fastapi/src/app/core/model.py
+++ python-fastapi/src/app/core/model.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 from sqlmodel import Field, SQLModel
 
-class ModelBase(SQLModel, table=True):
+class ModelBase(SQLModel):
     # The value of id will be None until we save it in the database, and then it will finally have a value.
     id: int | None = Field(default=None, primary_key=True)
     created_at: datetime = Field(default_factory=datetime.now)
