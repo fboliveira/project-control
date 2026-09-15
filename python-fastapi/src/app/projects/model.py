534f89974d5dd9e02d72bd186297871bde74ee5d
Fernando B Oliveira <fboliveira25@gmail.com>
2026-09-15 20:37:18 -0300

    fix: field

--- python-fastapi/src/app/projects/model.py
+++ python-fastapi/src/app/projects/model.py
@@ -1,12 +1,11 @@
 from sqlmodel import Relationship, SQLModel
 from app.core.model import ModelBase
-from app.tasks.model import Task
 
 class Project(ModelBase, table=True):
     __tablename__ = "tb_projects"
     name: str
 
-    tasks = list["Task"] = Relationship(back_populates="project")
+    tasks: list["Task"] = Relationship(back_populates="project")
 
 class CreateProject(SQLModel):
     name: str
