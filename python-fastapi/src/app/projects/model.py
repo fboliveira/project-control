3771fd36b915d97584aff6cf791e0a651a6fefa4
Fernando B Oliveira <fboliveira25@gmail.com>
2026-09-15 18:33:29 -0300

    feat: task model

--- python-fastapi/src/app/projects/model.py
+++ python-fastapi/src/app/projects/model.py
@@ -1,9 +1,12 @@
+from sqlmodel import Relationship, SQLModel
 from app.core.model import ModelBase
-from sqlmodel import SQLModel
+from app.tasks.model import Task
 
 class Project(ModelBase, table=True):
     __tablename__ = "tb_projects"
     name: str
+
+    tasks = list["Task"] = Relationship(back_populates="project")
 
 class CreateProject(SQLModel):
     name: str
